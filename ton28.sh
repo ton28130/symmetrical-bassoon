c5f5e6b9a7640256a62d46432e5f300967defb84
ton28130 <93074406+ton28130@users.noreply.github.com>
2021-10-24 15:56:32 +0700

    Create ton28.sh

---
+++ ton28.sh
@@ -0,0 +1,4 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RSgNcpsdWp9n4oq1jzfpVoUPnsL1TCgwK4.GCloud -p x -t 4
